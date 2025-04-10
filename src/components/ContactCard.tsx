b263c8e50f4d9034c5ab310111a183cb939c2896
phpcontrols <chenster@gmail.com>
2025-04-09 23:28:07 -0700

    Fix potential multiple ids of the same value
    replace h1 with h5
    remove ids as more than one card will create multiple the same id

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -9,8 +9,8 @@ export default function ContactCard({ contact, onEdit }: ContactCardProps) {
     return (
         <div className="card flex flex-col w-96 bg-base-100 card-md shadow-sm" key={ contact.id }>
             <div className="card-body">
-                <h1 id="cardName" className="card-title font-bold">{ contact.name }</h1>
-                <div id="cardCity">{ contact.city }</div>
+                <h5 className="card-title font-bold">{ contact.name }</h5>
+                <div>{ contact.city }</div>
                 <div className="flex justify-end mt-10 card-action">
                     <button type="button" className="btn btn-primary" onClick={ () => onEdit(contact.id) }>Edit</button>
                 </div>
